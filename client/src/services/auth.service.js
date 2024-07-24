27dd1400b1038821a3d681ef16899f2d45d2f0ea
Brown <monpp54712@gmail.com>
2024-07-25 02:53:05 +0800

    react頁面使用者登入功能實現

--- client/src/services/auth.service.js
+++ client/src/services/auth.service.js
@@ -2,8 +2,15 @@ import axios from "axios";
 const API_URL = "http://localhost:8080/api/user";
 
 class AuthService {
-  login() {}
-  logout() {}
+  login(email, password) {
+    // {email,password}表示要發送到伺服器的請求主體（request body）
+    return axios.post(API_URL + "/login", { email, password });
+  }
+  logout() {
+    localStorage.removeItem("user");
+  }
+
+  // 註冊帳號
   register(username, email, password, role) {
     return axios.post(API_URL + "/register", {
       username,
@@ -11,6 +18,11 @@ class AuthService {
       password,
       role,
     });
+  }
+
+  getCurrentUser() {
+    // 使用 JSON.parse 將這個 JSON 字符串解析為 JavaScript 對象
+    return JSON.parse(localStorage.getItem("user"));
   }
 }
 
