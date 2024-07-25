1ab4bc4c460a6576fc0d8173fc5b4a6c82a8ffb0
Brown <monpp54712@gmail.com>
2024-07-26 00:15:25 +0800

    react搜尋課程頁面及功能實現

---
+++ client/src/components/enroll-component.js
@@ -0,0 +1,92 @@
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import courseService from "../services/course.service";
+
+const EnrollComponent = ({ currentUser, setCurrentUser }) => {
+  const navigate = useNavigate();
+  let [searchInput, setSearchInput] = useState("");
+  let [searchResult, setSearchResult] = useState(null);
+
+  const handleTakeToLogin = () => {
+    navigate("/login");
+  };
+
+  const handleChangeInput = (e) => {
+    setSearchInput(e.target.value);
+  };
+
+  const handleSearch = () => {
+    courseService
+      .getCourseByName(searchInput)
+      .then((data) => {
+        setSearchResult(data.data);
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  };
+
+  return (
+    <div style={{ padding: "3rem" }}>
+      {!currentUser && (
+        <div>
+          <p>您必須先登入才能開始註冊課程</p>
+          <button
+            className="btn btn-primary btn-lg"
+            onClick={handleTakeToLogin}
+          >
+            回到登入頁面
+          </button>
+        </div>
+      )}
+      {currentUser && currentUser.user.role == "instructor" && (
+        <div>
+          <h1>只有學生才能夠註冊課程</h1>
+        </div>
+      )}
+
+      {currentUser && currentUser.user.role == "student" && (
+        <div className="search input-group mb-3">
+          <input
+            type="text"
+            className="form-control "
+            onChange={handleChangeInput}
+          ></input>
+          <button className="btn btn-primary" onClick={handleSearch}>
+            搜尋課程
+          </button>
+        </div>
+      )}
+
+      {currentUser && searchResult && searchResult.length != 0 && (
+        <div>
+          <p>這是我們從API返回的數據: </p>
+          {searchResult.map((course) => {
+            return (
+              <div key={course._id} className="card" style={{ width: "18rem" }}>
+                <div className="card-body">
+                  <h5 className="card-title">課程名稱{course.title}</h5>
+                  <p style={{ margin: "0.5rem 0rem" }}>{course.description}</p>
+                  <p style={{ margin: "0.5rem 0rem" }}>
+                    學生人數: {course.students.length}
+                  </p>
+                  <p style={{ margin: "0.5rem 0rem" }}>
+                    課程價格: {course.price}
+                  </p>
+                  <p style={{ margin: "0.5rem 0rem" }}>
+                    講師: {course.instructor.username}
+                  </p>
+                  <a href="#" className="card-tex btn btn-primary">
+                    註冊課程
+                  </a>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default EnrollComponent;
